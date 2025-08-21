a3f0a096f90fb90d36bd0c07cb6e2fd489152ffe
Borjis131 <borjainestahernandez@hotmail.com>
2025-08-21 17:36:59 +0000

    e1ap: add messages and types for the BC Bearer Context Setup procedure - e1ap_bc_bearer_context_setup_request, e1ap_bc_bearer_context_setup_response and e1ap_bc_bearer_context_setup_failure messages. - gnb_cu_cp_mbs_e1ap_id_t, gnb_cu_up_mbs_e1ap_id_t, e1ap_bc_f1u_context_reference_e1, e1ap_bc_mrb_setup_config, e1ap_mbs_ngu_information_at_5gc, e1ap_bc_bearer_context_ngu_tnl_info_at_5gc, e1ap_requested_action_for_available_shared_ngu_termination, e1ap_bc_bearer_context_to_setup, e1ap_bc_bearer_context_f1u_tnl_info_at_cu, e1ap_f1u_tnl_info_added_item, e1ap_bc_mrb_setup_response_item, e1ap_bc_mrb_failed_item, e1ap_mbs_ngu_information_at_ngran, e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran, e1ap_bc_bearer_context_to_setup_response auxiliar types and functions

--- include/srsran/e1ap/common/e1ap_types.h
+++ include/srsran/e1ap/common/e1ap_types.h
@@ -30,6 +30,7 @@
 #include "srsran/ran/rb_id.h"
 #include "srsran/ran/up_transport_layer_info.h"
 #include "srsran/security/security.h"
+#include "srsran/ran/mbs.h"
 #include <cstdint>
 
 namespace srsran {
@@ -64,6 +65,54 @@ constexpr uint64_t gnb_cu_up_ue_e1ap_id_to_uint(gnb_cu_up_ue_e1ap_id_t id)
 constexpr gnb_cu_up_ue_e1ap_id_t int_to_gnb_cu_up_ue_e1ap_id(uint64_t idx)
 {
   return static_cast<gnb_cu_up_ue_e1ap_id_t>(idx);
+}
+
+/// \brief gNB-CU-CP MBS E1AP ID used to identify the MBS context in the CU-CP E1AP.
+/// \remark See TS 37.483 Section 9.3.1.106: gNB-CU-CP MBS E1AP ID valid values: (0..2^24-1)
+static constexpr uint32_t MAX_NOF_CU_CP_E1AP_MBS_IDS = ((uint32_t)1 << 24);
+enum class gnb_cu_cp_mbs_e1ap_id_t : uint32_t { min = 0, max = MAX_NOF_CU_CP_E1AP_MBS_IDS - 1, invalid = MAX_NOF_CU_CP_E1AP_MBS_IDS };
+
+constexpr uint32_t gnb_cu_cp_mbs_e1ap_id_to_uint(gnb_cu_cp_mbs_e1ap_id_t id)
+{
+  return static_cast<uint32_t>(id);
+}
+
+/// Convert integer to gNB-CU-CP MBS E1AP ID type.
+constexpr gnb_cu_cp_mbs_e1ap_id_t uint_to_gnb_cu_cp_mbs_e1ap_id(uint32_t idx)
+{
+  return static_cast<gnb_cu_cp_mbs_e1ap_id_t>(idx);
+}
+
+/// \brief gNB-CU-UP MBS E1AP ID used to identify the MBS context in the CU-UP E1AP.
+/// \remark See TS 37.483 Section 9.3.1.107: gNB-CU-UP MBS E1AP ID valid values: (0..2^16-1)
+static constexpr uint32_t MAX_NOF_CU_UP_E1AP_MBS_IDS = ((uint32_t)1 << 16);
+enum class gnb_cu_up_mbs_e1ap_id_t : uint32_t { min = 0, max = MAX_NOF_CU_UP_E1AP_MBS_IDS - 1, invalid = MAX_NOF_CU_UP_E1AP_MBS_IDS };
+
+constexpr uint32_t gnb_cu_up_mbs_e1ap_id_to_uint(gnb_cu_up_mbs_e1ap_id_t id)
+{
+  return static_cast<uint32_t>(id);
+}
+
+/// Convert integer to gNB-CU-UP MBS E1AP ID type.
+constexpr gnb_cu_up_mbs_e1ap_id_t uint_to_gnb_cu_up_mbs_e1ap_id(uint32_t idx)
+{
+  return static_cast<gnb_cu_up_mbs_e1ap_id_t>(idx);
+}
+
+/// \brief Broadcast F1-U Context ReferenceE1 used to identify the Broadcast F1-U context within an MBS-associated logical E1-connection.
+/// \remark See TS 37.483 Section 9.3.1.139a: Broadcast F1-U Context ReferenceE1 valid values: (0..2^16-1)
+static constexpr uint32_t MAX_NOF_F1U_CONTEXT_REFERENCE_E1 = ((uint32_t)1 << 16);
+enum class e1ap_bc_f1u_context_reference_e1 : uint32_t { min = 0, max = MAX_NOF_F1U_CONTEXT_REFERENCE_E1 - 1, invalid = MAX_NOF_F1U_CONTEXT_REFERENCE_E1 };
+
+constexpr uint32_t e1ap_bc_f1u_context_reference_e1_to_uint(e1ap_bc_f1u_context_reference_e1 id)
+{
+  return static_cast<uint32_t>(id);
+}
+
+/// Convert integer to Broadcast F1-U Context ReferenceE1 type.
+constexpr e1ap_bc_f1u_context_reference_e1 uint_to_e1ap_bc_f1u_context_reference_e1(uint32_t idx)
+{
+  return static_cast<e1ap_bc_f1u_context_reference_e1>(idx);
 }
 
 enum class e1ap_ul_cfg { no_data = 0, shared, only };
@@ -208,10 +257,17 @@ struct e1ap_up_params_item {
   uint8_t                 cell_group_id;
 };
 
+/// See TS 37.483 Section 9.3.1.12: QoS Flow List
 struct e1ap_qos_flow_item {
+  //qos_flow_id (M)
   qos_flow_id_t qos_flow_id = qos_flow_id_t::invalid;
+  //qos_flow_mapping_indication (O)
+  //std::optional<qos_flow_mapping_indication>
+  //data_forwarding_source_ip_address (O)
+  //std::optional<data_forwarding_source_ip_address>
 };
 
+/// See TS 37.483 Section 9.3.1.45: Flow Failed List
 struct e1ap_qos_flow_failed_item {
   qos_flow_id_t qos_flow_id = qos_flow_id_t::invalid;
   e1ap_cause_t  cause;
@@ -332,6 +388,199 @@ struct e1ap_pdu_session_resource_modified_item {
   slotted_id_vector<drb_id_t, e1ap_drb_failed_item_ng_ran>   drb_failed_to_modify_list_ng_ran;
   std::optional<security_result_t>                           security_result;
   std::optional<e1ap_data_forwarding_info>                   pdu_session_data_forwarding_info_resp;
+};
+
+/// See TS 37.483 Section 9.3.1.114: BC MRB Setup Configuration
+struct e1ap_bc_mrb_setup_config {
+  //mrb_id (M)
+  mrb_id_t mrb_id = mrb_id_t::invalid;
+  //mbs_pdcp_configuration (M)
+  e1ap_pdcp_config mbs_pdcp_cfg;
+  //mbs_qos_flows_information_to_be_setup (M)
+  std::vector<e1ap_qos_flow_qos_param_item> mbs_qos_flow_info_to_be_setup;
+  //mrb_qos (O)
+  std::optional<e1ap_qos_flow_level_qos_params> mrb_qos;
+  //f1u_tnl_info_to_add_list (0..1)
+  std::vector<e1ap_bc_f1u_context_reference_e1> f1u_tnl_info_to_add_list;
+};
+
+// NOTE (borieher): In the spec is a choice type with only one option
+/// \brief MBS NG-U Information at 5GC.
+/// \remark See TS 37.483, 9.3.1.113 - MBS NG-U Information at 5GC.
+struct e1ap_mbs_ngu_information_at_5gc {
+  //ip_multicast_address (M)
+  transport_layer_address ip_multicast_address;
+  //ip_source_address (M)
+  transport_layer_address ip_source_address;
+  //gtp_dl_teid (M)
+  gtpu_teid_t gtp_dl_teid;
+};
+
+struct e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_dependent_item {
+  //mbs_area_session_id (M)
+  area_session_id_t mbs_area_session_id;
+  //mbs_ngu_information_at_5gc (M)
+  e1ap_mbs_ngu_information_at_5gc mbs_ngu_information_at_5gc;
+};
+
+struct e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_independent {
+  //mbs_ngu_information_at_5gc (M)
+  e1ap_mbs_ngu_information_at_5gc mbs_ngu_information_at_5gc;
+};
+
+struct e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_dependent {
+  //location_dependent_mbs_ngu_info_at_5gc (1..maxnoofMBSAreaSessionIDs)
+  std::vector<e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_dependent_item> location_dependent_mbs_ngu_info_at_5gc;
+};
+
+
+/// \brief BC Bearer Context NG-U TNL Info at 5GC.
+/// \remark See TS 37.483, 9.3.1.112 - BC Bearer Context NG-U TNL Info at 5GC.
+struct e1ap_bc_bearer_context_ngu_tnl_info_at_5gc {
+  e1ap_bc_bearer_context_ngu_tnl_info_at_5gc() : choice(e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_dependent{}) {}
+  e1ap_bc_bearer_context_ngu_tnl_info_at_5gc(const e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_independent& val) : choice(val) {}
+  e1ap_bc_bearer_context_ngu_tnl_info_at_5gc(const e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_dependent& val) : choice(val) {}
+
+  bool is_locationdependent() const { return std::holds_alternative<e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_dependent>(choice); }
+
+  e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_independent get_locationindependent() { return std::get<e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_independent>(choice); }
+  e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_dependent get_locationdependent() { return std::get<e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_dependent>(choice); }
+
+private:
+  std::variant<e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_independent, e1ap_bc_bearer_ctxt_ngu_tnl_info_at_5gc_location_dependent> choice;
+};
+
+/// See TS 37.483 Section 9.3.1.115: Requested Action for Available Shared NG-U Termination
+enum class e1ap_requested_action_for_available_shared_ngu_termination : uint8_t {
+  apply_available_config = 0,
+  apply_requested_config,
+  apply_available_config_if_same_as_requested,
+  unspecified
+};
+
+/// See TS 37.483 Section 9.3.3.26: BC Bearer Context To Setup
+struct e1ap_bc_bearer_context_to_setup {
+  //s_nssai (M)
+  s_nssai_t s_nssai;
+  //bc_bearer_context_ngu_tnl_info_at_5gc (O)
+  std::optional<e1ap_bc_bearer_context_ngu_tnl_info_at_5gc> bc_bearer_context_ngu_tnl_info_at_5gc;
+  //bc_mrb_to_setup_list (M)
+  std::vector<e1ap_bc_mrb_setup_config> bc_mrb_to_setup_list;
+  //requested_action_for_available_shared_ngu_termination (O)
+  std::optional<e1ap_requested_action_for_available_shared_ngu_termination> requested_action_for_available_shared_ngu_termination;
+};
+
+struct e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_dependent_item {
+  //mbs_area_session_id (M)
+  area_session_id_t mbs_area_session_id;
+  //mbs_f1u_information_at_cu (M)
+  up_transport_layer_info mbs_f1u_information_at_cu;
+};
+
+struct e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_independent {
+  //mbs_f1u_information_at_cu (M)
+  up_transport_layer_info mbs_f1u_information_at_cu;
+};
+
+struct e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_dependent {
+  //location_dependent_mbs_f1u_information_at_cu (1..maxnoofMBSAreaSessionIDs)
+  std::vector<e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_dependent_item> location_dependent_mbs_f1u_information_at_cu;
+};
+
+/// \brief BC Bearer Context F1-U TNL Info at CU.
+/// \remark See TS 37.483, 9.3.1.118 - BC Bearer Context F1-U TNL Info at CU.
+struct e1ap_bc_bearer_context_f1u_tnl_info_at_cu {
+  e1ap_bc_bearer_context_f1u_tnl_info_at_cu() : choice(e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_dependent{}) {}
+  e1ap_bc_bearer_context_f1u_tnl_info_at_cu(const e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_independent& val) : choice(val) {}
+  e1ap_bc_bearer_context_f1u_tnl_info_at_cu(const e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_dependent& val) : choice(val) {}
+
+  bool is_locationdependent() const { return std::holds_alternative<e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_dependent>(choice); }
+
+  e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_independent get_locationindependent() { return std::get<e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_independent>(choice); }
+  e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_dependent get_locationdependent() { return std::get<e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_dependent>(choice); }
+
+private:
+  std::variant<e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_independent, e1ap_bc_bearer_ctxt_f1u_tnl_at_cu_location_dependent> choice;
+};
+
+struct e1ap_f1u_tnl_info_added_item {
+  //bc_f1u_context_reference_e1 (M)
+  e1ap_bc_f1u_context_reference_e1 bc_f1u_context_reference_e1;
+  //bc_bearer_context_f1u_tnl_info_at_cu (M)
+  e1ap_bc_bearer_context_f1u_tnl_info_at_cu bc_bearer_context_f1u_tnl_info_at_cu;
+};
+
+struct e1ap_bc_mrb_setup_response_item {
+  //mrb_id (M)
+  mrb_id_t mrb_id = mrb_id_t::invalid;
+  //mbs_qos_flow_setup_list (M)
+  std::vector<e1ap_qos_flow_item> mbs_qos_flow_setup_list;
+  //mbs_qos_flow_failed_list (O)
+  std::optional<std::vector<e1ap_qos_flow_failed_item>> mbs_qos_flow_failed_list;
+  //bc_bearer_context_f1u_tnl_info_at_cu (M)
+  e1ap_bc_bearer_context_f1u_tnl_info_at_cu bc_bearer_context_f1u_tnl_info_at_cu;
+  //f1u_tnl_info_added_list (0..1)
+  std::vector<e1ap_f1u_tnl_info_added_item> f1u_tnl_info_added_list;
+};
+
+struct e1ap_bc_mrb_failed_item {
+  //mrb_id (M)
+  mrb_id_t mrb_id = mrb_id_t::invalid;
+  //cause (M)
+  e1ap_cause_t cause;
+};
+
+// NOTE (borieher): In the spec is a choice type with only one option
+/// \brief MBS NG-U Information at NG-RAN.
+/// \remark See TS 37.483, 9.3.1.117 - MBS NG-U Information at NG-RAN.
+struct e1ap_mbs_ngu_information_at_ngran {
+  //shared_ngu_dl_transport_layer_info (M)
+  up_transport_layer_info shared_ngu_dl_transport_layer_info;
+};
+
+struct e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_dependent_item {
+  //mbs_area_session_id (M)
+  area_session_id_t mbs_area_session_id;
+  //mbs_ngu_information_at_ngran (M)
+  e1ap_mbs_ngu_information_at_ngran mbs_ngu_information_at_ngran;
+};
+
+struct e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_independent {
+  //mbs_ngu_information_at_ngran (M)
+  e1ap_mbs_ngu_information_at_ngran mbs_ngu_information_at_ngran;
+};
+
+struct e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_dependent {
+  //location_dependent_mbs_ngu_information_at_ngran (1..maxnoofMBSAreaSessionIDs)
+  std::vector<e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_dependent_item> location_dependent_mbs_ngu_information_at_ngran;
+};
+
+/// \brief BC Bearer Context NG-U TNL Info at NG-RAN.
+/// \remark See TS 37.483, 9.3.1.116 - BC Bearer Context NG-U TNL Info at NG-RAN.
+struct e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran {
+  e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran() : choice(e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_dependent{}) {}
+  e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran(const e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_independent& val) : choice(val) {}
+  e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran(const e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_dependent& val) : choice(val) {}
+
+  bool is_locationdependent() const { return std::holds_alternative<e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_dependent>(choice); }
+
+  e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_independent get_locationindependent() { return std::get<e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_independent>(choice); }
+  e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_dependent get_locationdependent() { return std::get<e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_dependent>(choice); }
+
+private:
+  std::variant<e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_independent, e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran_location_dependent> choice;
+};
+
+/// See TS 37.483 Section 9.3.3.27: BC Bearer Context To Setup Response
+struct e1ap_bc_bearer_context_to_setup_response {
+  //bc_bearer_context_ngu_tnl_info_at_ng_ran (O)
+  std::optional<e1ap_bc_bearer_context_ngu_tnl_info_at_ng_ran> bc_bearer_context_ngu_tnl_info_at_ng_ran;
+  //bc_mrb_setup_response_list (1..maxnoofMRBs)
+  std::vector<e1ap_bc_mrb_setup_response_item> bc_mrb_setup_response_list;
+  //bc_mrb_failed_list (0..maxnoofMRBs)
+  std::vector<e1ap_bc_mrb_failed_item> bc_mrb_failed_list;
+  //available_bc_mrb_configuration (O)
+  std::optional<std::vector<e1ap_bc_mrb_setup_config>> available_bc_mrb_configuration;
 };
 
 } // namespace srsran
---
+++ include/srsran/e1ap/cu_cp/e1ap_cu_cp_bc_bearer_context_setup.h
@@ -0,0 +1,65 @@
+/*
+ * 5G-MAG Reference Tools
+ * Copyright (C) 2025 iTEAM UPV <borieher@iteam.upv.es>
+ *
+ * This file is part of srsRAN.
+ *
+ * srsRAN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * srsRAN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * A copy of the GNU Affero General Public License can be found in
+ * the LICENSE file in the top-level directory of this distribution
+ * and at http://www.gnu.org/licenses/.
+ *
+ */
+
+#pragma once
+
+#include "srsran/cu_cp/cu_cp_types.h"
+#include "srsran/e1ap/common/e1ap_types.h"
+#include "srsran/ran/mbs.h"
+
+namespace srsran {
+namespace srs_cu_cp {
+
+struct e1ap_bc_bearer_context_setup_request {
+  mbs_index_t mbs_index = mbs_index_t::invalid;
+  //gnb_cu_cp_mbs_e1ap_id (M)
+  gnb_cu_cp_mbs_e1ap_id_t   gnb_cu_cp_mbs_e1ap_id;
+  //global_mbs_session_id (M)
+  mbs_session_id_t          global_mbs_session_id;
+  //bc_bearer_context_to_setup (M)
+  e1ap_bc_bearer_context_to_setup bc_bearer_context_to_setup;
+};
+
+struct e1ap_bc_bearer_context_setup_response {
+  //gnb_cu_cp_mbs_e1ap_id (M)
+  gnb_cu_cp_mbs_e1ap_id_t gnb_cu_cp_mbs_e1ap_id;
+  //gnb_cu_up_mbs_e1ap_id (M)
+  gnb_cu_up_mbs_e1ap_id_t gnb_cu_up_mbs_e1ap_id;
+  //bc_bearer_context_to_setup_response (M)
+  e1ap_bc_bearer_context_to_setup_response bc_bearer_context_to_setup_response;
+  //crit_diagnostics (O)
+  std::optional<e1ap_crit_diagnostics> crit_diagnostics;
+};
+
+struct e1ap_bc_bearer_context_setup_failure {
+  //gnb_cu_cp_mbs_e1ap_id (M)
+  gnb_cu_cp_mbs_e1ap_id_t gnb_cu_cp_mbs_e1ap_id;
+  //gnb_cu_up_mbs_e1ap_id (O)
+  std::optional<gnb_cu_up_mbs_e1ap_id_t> gnb_cu_up_mbs_e1ap_id;
+  //cause (M)
+  e1ap_cause_t cause;
+  //crit_diagnostics (O)
+  std::optional<e1ap_crit_diagnostics> crit_diagnostics;
+};
+
+} // namespace srs_cu_cp
+} // namespace srsran
